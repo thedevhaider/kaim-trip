21334328655d525f45822a5a83e610caeb0d7ef6
Haider <lonewolf@Haiders-MacBook-Air.local>
2020-08-12 22:56:29 +0530

    Some changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,7 +46,7 @@ router.post("/login", (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 3600 },
+          { expiresIn: 86400 },
           (err, token) => {
             if (err) throw err;
             res.json({
