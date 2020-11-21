38fb29fafd3bd45bd0ad690b3304a0239dcabe5a
Haider <haider@livbim.io>
2020-11-21 20:13:15 +0530

    Build be used for production env only

--- server.js
+++ server.js
@@ -55,10 +55,12 @@ app.use("/api/destinations", destinations);
 app.use("/api/contacts", contacts);
 app.use("/api/places", places);
 
+if (process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}
 
 const PORT = process.env.PORT || 5000;
 
