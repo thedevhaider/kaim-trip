93c624f3a045396a62b6273e5a7988619c1946e9
Haider <haider@livbim.io>
2020-11-21 14:43:37 +0530

    Removed Build files from client gitignore

---
+++ client/build/precache-manifest.6344b7e64dfe0115fbad25b9222e4ee9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e6a33c8916a754ced0d1acbb9976f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47da1b964559ca5340dd",
+    "url": "/static/css/2.c2a24287.chunk.css"
+  },
+  {
+    "revision": "4a95cc03daa90bc80b27",
+    "url": "/static/css/main.5f72a7b2.chunk.css"
+  },
+  {
+    "revision": "47da1b964559ca5340dd",
+    "url": "/static/js/2.e36b182b.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/static/js/2.e36b182b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a95cc03daa90bc80b27",
+    "url": "/static/js/main.4d4b3be8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
