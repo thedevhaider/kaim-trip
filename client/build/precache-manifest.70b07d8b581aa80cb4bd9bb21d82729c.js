41c079319388aea3cc79328b81e840c6e9fa7157
Mohit Gupta <mohit.gupta30000@gmail.com>
2020-11-25 00:26:09 +0530

    Added overlay

---
+++ client/build/precache-manifest.70b07d8b581aa80cb4bd9bb21d82729c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2d0a72a73b2c5a3c67de95d0b92c34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50cbe02c5a2647892d94",
+    "url": "/static/css/2.c2a24287.chunk.css"
+  },
+  {
+    "revision": "6fef23a13df292343f39",
+    "url": "/static/css/main.07c22266.chunk.css"
+  },
+  {
+    "revision": "50cbe02c5a2647892d94",
+    "url": "/static/js/2.e2b4cf5f.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/static/js/2.e2b4cf5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fef23a13df292343f39",
+    "url": "/static/js/main.79c7c6b3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
