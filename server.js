15682eb38c992f2ccf3791c21f116d0e1f713218
Haider <haider@livbim.io>
2020-12-02 23:22:19 +0530

    Changed server port to 80

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ if (process.env.NODE_ENV === "production"){
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => console.log(`> Running server on Port ${PORT}`));
